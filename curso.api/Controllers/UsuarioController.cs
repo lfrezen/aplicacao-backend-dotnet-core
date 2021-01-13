c470f138ee39c51e39af290131a8a3bae4407095
Lucian Rezende <lucianfiocello@gmail.com>
2021-01-12 21:49:06 -0300

    Add UsuarioController e configurado o Swagger no projeto

---
+++ curso.api/Controllers/UsuarioController.cs
@@ -0,0 +1,34 @@
+﻿using curso.api.Models.Usuarios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace curso.api.Controllers
+{
+    [Route("api/v1/usuario")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        /// <summary>
+        /// teste
+        /// </summary>
+        /// <param name="loginViewModelInput"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("logar")]
+        public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+        {
+            return Ok(loginViewModelInput);
+        }
+
+        [HttpPost]
+        [Route("registrar")]
+        public IActionResult Registrar(RegistroViewModelInput registroViewModelInput)
+        {
+            return Created("", registroViewModelInput);
+        }
+    }
+}
