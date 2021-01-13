35ecfef32a1ba17dfbc87a42dbf2bf759c2e4956
Lucian Rezende <lucianfiocello@gmail.com>
2021-01-13 00:51:14 -0300

    Corrigido erro do Swagger, incluido EF Core, Migrations e Mappings

---
+++ curso.api/Controllers/CursoController.cs
@@ -0,0 +1,46 @@
+﻿using curso.api.Models.Cursos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace curso.api.Controllers
+{
+    [Route("api/v1/cursos")]
+    [ApiController]
+    [Authorize]
+    public class CursoController : ControllerBase
+    {
+        [SwaggerResponse(statusCode: 200, description: "Sucesso ao obter os", Type = typeof(CursoViewModelInput))]
+        [SwaggerResponse(statusCode: 401, description: "Não autorizado")]
+        [HttpPost]
+        [Route("")]
+        public async Task<IActionResult> Post(CursoViewModelInput cursoViewModelInput)
+        {
+            var codigoUsuario = int.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
+            return Created("", cursoViewModelInput);
+        }
+
+        [SwaggerResponse(statusCode: 200, description: "Sucesso ao autenticar", Type = typeof(CursoViewModelInput))]
+        [SwaggerResponse(statusCode: 401, description: "Não autorizado")]
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> Get(CursoViewModelOutput cursoViewModelOutput)
+        {
+            var cursos = new List<CursoViewModelOutput>();
+            //var codigoUsuario = int.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
+            cursos.Add(new CursoViewModelOutput()
+            {
+                Login = "",
+                Descricao = "Teste",
+                Nome = "Teste"
+            });            
+            return Ok(cursos);
+        }
+    }
+}
--- curso.api/Controllers/UsuarioController.cs
+++ curso.api/Controllers/UsuarioController.cs
@@ -1,13 +1,17 @@
-﻿using curso.api.Filters;
+﻿using curso.api.Business.Entities;
+using curso.api.Filters;
+using curso.api.Infraestrutura.Data;
 using curso.api.Models;
 using curso.api.Models.Usuarios;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.Annotations;
 using System;
-using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Security.Claims;
+using System.Text;
 
 namespace curso.api.Controllers
 {
@@ -28,7 +32,35 @@ namespace curso.api.Controllers
         [ValidacaoModelStateCustomizado]
         public IActionResult Logar(LoginViewModelInput loginViewModelInput)
         {
-            return Ok(loginViewModelInput);
+            var usuarioViewModelOutput = new UsuarioViewModelOutput()
+            {
+                Codigo = 1,
+                Login = "usuario",
+                Email = "usuario@email.com"
+            };
+
+            var secret = Encoding.ASCII.GetBytes("123456");
+            var symmetricSecurityKey = new SymmetricSecurityKey(secret);
+            var securityTokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, usuarioViewModelOutput.Codigo.ToString()),
+                    new Claim(ClaimTypes.Name, usuarioViewModelOutput.Login.ToString()),
+                    new Claim(ClaimTypes.Email, usuarioViewModelOutput.Email.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var tokenGenerated = jwtSecurityTokenHandler.CreateToken(securityTokenDescriptor);
+            var token = jwtSecurityTokenHandler.WriteToken(tokenGenerated);
+
+            return Ok(new 
+            { 
+                Token = token,
+                Usuario = usuarioViewModelOutput
+            });
         }
 
         [HttpPost]
@@ -36,6 +68,23 @@ namespace curso.api.Controllers
         [ValidacaoModelStateCustomizado]
         public IActionResult Registrar(RegistroViewModelInput registroViewModelInput)
         {
+            var optionsBuilder = new DbContextOptionsBuilder<CursoDbContext>();
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=curso;Integrated Security=True");
+            CursoDbContext contexto = new CursoDbContext(optionsBuilder.Options);
+
+            var migracoesPendentes = contexto.Database.GetPendingMigrations();
+            if (migracoesPendentes.Count() > 0)
+            {
+                contexto.Database.Migrate();
+            }
+            
+            var usuario = new Usuario();
+            usuario.Login = registroViewModelInput.Login;
+            usuario.Senha = registroViewModelInput.Senha;
+            usuario.Email = registroViewModelInput.Email;
+            contexto.Usuario.Add(usuario);
+            contexto.SaveChanges();
+
             return Created("", registroViewModelInput);
         }
     }
